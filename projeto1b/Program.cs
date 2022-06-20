c8d82dba4e534558773759a16edba69878938102
Xapeleta <viniciassouza@gmail.com>
2022-06-20 20:06:48 -0300

    criacao pasta mclan

--- projeto1b/Program.cs
+++ projeto1b/Program.cs
@@ -10,6 +10,15 @@ namespace projeto1b
     {
         static void Main(string[] args)
         {
+            int numero;
+            mclan aluno;
+            aluno = new mclan();
+            mclan professor = new mclan();
+            aluno.nome = "iguinho";
+            professor.nome = "prof.iguinhotenhofuego";
+            Console.WriteLine("aluno:" + aluno.nome);
+            Console.WriteLine("professor:" + professor.nome);
+            Console.ReadKey();
         }
     }
 }
